2ef8a227d400210f53912bdc274fd8ac76ffdced
Victor Yuri <victoryuritc@yahoo.com>
2023-02-20 16:07:40 -0300

    feat: complete Footer for desktop version

--- src/atomic/molecules/WhatsAppContactIcon/WhatsAppContactIcon.tsx
+++ src/atomic/molecules/WhatsAppContactIcon/WhatsAppContactIcon.tsx
@@ -6,47 +6,59 @@ function WhatsAppContactIcon() {
   const WHATSAPP_COLOR = "#25D366";
   const [isHoveringWhatsApp, setIsHoveringWhatsApp] = useState(false);
   return (
-    <a
-      href="https://api.whatsapp.com/send/?phone=553195767298&text&type=phone_number&app_absent=0"
-      target="_blank"
-      rel="noreferrer"
+    <div
       className="d-flex align-items-center position-fixed bottom-0 end-0 d-flex text-decoration-none"
-      onMouseEnter={() => setIsHoveringWhatsApp(true)}
-      onMouseLeave={() => setIsHoveringWhatsApp(false)}
       style={{
         zIndex: "150",
       }}>
-      {
-        <div
-          style={{
-            backgroundColor: WHATSAPP_COLOR,
-            borderRadius: "1em",
-            height: "2em",
-            marginBottom: "1em",
-            color: Colors.WHITE,
-            opacity: isHoveringWhatsApp ? "1.0" : "0",
-            visibility: isHoveringWhatsApp ? "visible" : "hidden",
-            transition: "400ms",
-            scale: isHoveringWhatsApp ? "1.0" : "0",
-          }}
-          className="d-flex align-items-center justify-content-center px-4 py-2 me-1">
-          <span>Fale conosco</span>
-        </div>
-      }
-
-      <Whatsapp
+      <a
+        href="https://api.whatsapp.com/send/?phone=553195767298&text&type=phone_number&app_absent=0"
+        target="_blank"
+        rel="noreferrer"
+        onMouseEnter={() => {
+          isHoveringWhatsApp
+            ? setIsHoveringWhatsApp(true)
+            : setIsHoveringWhatsApp(false);
+        }}
+        onMouseLeave={() => {
+          setIsHoveringWhatsApp(false);
+        }}
         style={{
           backgroundColor: WHATSAPP_COLOR,
           borderRadius: "1em",
-          padding: "0.5em",
-          marginRight: "1em",
+          height: "2em",
           marginBottom: "1em",
+          color: Colors.WHITE,
+          opacity: isHoveringWhatsApp ? "1.0" : "0",
+          visibility: isHoveringWhatsApp ? "visible" : "hidden",
+          transition: "400ms",
+          scale: isHoveringWhatsApp ? "1.0" : "0",
         }}
-        color={Colors.WHITE}
-        width={60}
-        height={60}
-      />
-    </a>
+        className="text-decoration-none d-flex align-items-center justify-content-center px-4 py-2 me-1">
+        <span>Fale conosco</span>
+      </a>
+
+      <a
+        href="https://api.whatsapp.com/send/?phone=553195767298&text&type=phone_number&app_absent=0"
+        target="_blank"
+        rel="noreferrer">
+        {" "}
+        <Whatsapp
+          onMouseEnter={() => setIsHoveringWhatsApp(true)}
+          onMouseLeave={() => setIsHoveringWhatsApp(false)}
+          style={{
+            backgroundColor: WHATSAPP_COLOR,
+            borderRadius: "0.5em",
+            padding: "0.4em",
+            marginRight: "1em",
+            marginBottom: "1em",
+          }}
+          color={Colors.WHITE}
+          width={48}
+          height={48}
+        />
+      </a>
+    </div>
   );
 }
 export default WhatsAppContactIcon;
---
+++ src/atomic/organisms/Footer/FooterLink.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Colors from "../../../foundations/Colors/Colors";
+
+interface IFooterLink {
+  to: string;
+  text: string;
+}
+
+function FooterLink({ to, text }: IFooterLink) {
+  const [isHoveringLink, setIsHoveringLink] = useState(false);
+  return (
+    <Link
+      to={to}
+      className="d-block"
+      onMouseEnter={() => setIsHoveringLink(true)}
+      onMouseLeave={() => setIsHoveringLink(false)}
+      style={{
+        color: isHoveringLink ? Colors.SECONDARY : Colors.BLACK,
+        fontSize: "calc(17px + 0.1vw)",
+        textDecoration: isHoveringLink ? "underline" : "none",
+      }}>
+      {text}
+    </Link>
+  );
+}
+export default FooterLink;
